22dce578d4ca51431a63689b35e0d9c82edaa09b
ahmedfyaz <awanahmed755@gmail.com>
2025-11-23 22:45:39 +0500

    started making chat bot

---
+++ Project2/main.py
@@ -0,0 +1,7 @@
+print("Hello I'm Albert, how can I help you Today? ")
+
+while True:
+    user_input = input("You : ")
+    if user_input=="exit"
+        break
+    print(f"cool thanks for sharing that {user_input}")

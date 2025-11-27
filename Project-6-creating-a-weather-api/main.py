287ceb994a52c6162589f705b5bb73b6abf33d3d
ahmedfyaz <awanahmed755@gmail.com>
2025-11-27 22:54:26 +0500

    completed todays shit

--- Project-6-creating-a-weather-api/main.py
+++ Project-6-creating-a-weather-api/main.py
@@ -5,4 +5,12 @@ app =Flask('Website')
 @app.route("/")
 def home():
     return render_template("home.html")
+@app.route("/api/v1/<station>/<date>")
+def about(station,date):
+    temperature = 23
+    return {
+        "station": station,
+        "data":date ,
+        "temperature":temperature
+    }
 app.run(debug=True)

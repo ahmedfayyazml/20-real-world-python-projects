d160c8d2886b64933171fee0137d13526dcc48e6
ahmedfyaz <awanahmed755@gmail.com>
2025-11-28 23:24:14 +0500

    api server is working fine shyt

--- Project-6-creating-a-weather-api/main.py
+++ Project-6-creating-a-weather-api/main.py
@@ -13,7 +13,9 @@ def about():
 
 @app.route("/api/v1/<station>/<date>")
 def api_station(station,date):
-    temperature = 23
+    filename = "data_small/TG_STAID" +str(station).zfill(6)+".txt"
+    df = pd.read_csv(filename,skiprows=20,parse_dates=["    DATE"])
+    temperature = df.loc[df['    DATE']==date]['   TG'].squeeze()/10
     return {
         "temperature": temperature,
         "station": station,

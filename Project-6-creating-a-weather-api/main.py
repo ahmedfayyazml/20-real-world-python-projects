ce3ec2d69a7313c80293b97e9c27064c361b051b
ahmedfyaz <awanahmed755@gmail.com>
2025-11-28 19:34:58 +0500

    made my inital server

--- Project-6-creating-a-weather-api/main.py
+++ Project-6-creating-a-weather-api/main.py
@@ -1,16 +1,23 @@
-from flask import Flask,render_template
-import pandas as pd
-app =Flask('Website')
+from flask import Flask, render_template
 
+import  pandas as pd
+
+app = Flask(__name__)
 @app.route("/")
 def home():
     return render_template("home.html")
+
+app.route("/about")
+def about():
+    return  render_template("about.me")
+
 @app.route("/api/v1/<station>/<date>")
-def about(station,date):
+def api_station(station,date):
     temperature = 23
     return {
+        "temperature": temperature,
         "station": station,
-        "data":date ,
-        "temperature":temperature
+        "date":date
     }
-app.run(debug=True)
+if __name__ == "__main__":
+    app.run(debug=True)

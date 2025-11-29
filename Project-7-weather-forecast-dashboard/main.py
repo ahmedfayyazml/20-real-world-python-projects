157f9284c93f438716296e4a6a239e4d5754ab97
ahmedfyaz <awanahmed755@gmail.com>
2025-11-29 21:57:27 +0500

    created the gui

--- Project-7-weather-forecast-dashboard/main.py
+++ Project-7-weather-forecast-dashboard/main.py
@@ -1,1 +1,8 @@
-import streamlit as st
+import  streamlit as st
+st.title("Weather Forecast for the Next Days")
+place = st.text_input("Place:",placeholder="London")
+days = st.slider("Forecast Days:",max_value=5,min_value=1,
+                 help="Select the number of forecasted days")
+option = st.selectbox("Select data to view:",
+                      ("Temperature","Sky"))
+st.subheader(f"{option} for the next {days} in {place}")

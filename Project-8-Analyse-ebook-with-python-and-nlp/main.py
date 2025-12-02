15a708bfe141e29dc431c235d6579395d5070df5
ahmedfyaz <awanahmed755@gmail.com>
2025-12-02 22:31:15 +0500

    run loop

--- Project-8-Analyse-ebook-with-python-and-nlp/main.py
+++ Project-8-Analyse-ebook-with-python-and-nlp/main.py
@@ -7,3 +7,11 @@ import  re
 pattern = re.compile("Chapter [0-9]+")
 chapters = re.split(pattern,book)
 chapters = chapters[1:]
+
+## Finding the chapters of the book that are negative and positive
+
+from nltk.sentiment import  SentimentIntensityAnalyzer
+analyzer = SentimentIntensityAnalyzer()
+
+## running loop to find
+

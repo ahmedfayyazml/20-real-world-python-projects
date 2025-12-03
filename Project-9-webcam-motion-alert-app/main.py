44c71ce5ecab838b744f8f870376c755fdcf15a0
ahmedfyaz <awanahmed755@gmail.com>
2025-12-03 23:54:48 +0500

    put countours

--- Project-9-webcam-motion-alert-app/main.py
+++ Project-9-webcam-motion-alert-app/main.py
@@ -3,15 +3,32 @@ os.environ["QT_QPA_PLATFORM"] = "xcb" #you dont have to put these
 import time
 import  cv2
 
-video = cv2.VideoCapture(0) # 0 of you are using your computer or laptop camera and 1 if external
+video = cv2.VideoCapture(0) # 0 of you are using your computer or laptop camera and 1 or 2 if external
 time.sleep(1) ## it will negate the black color in the starting and help the camera load
-
+first_frame = None
 while True:
     check,frame = video.read()
     gray_frame = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY) #convert the frame color into grey because it is less heavy
     gray_frame_gau = cv2.GaussianBlur(gray_frame, (21, 21),0)  # applying gaussian blur to make calculations more efficient
-    cv2.imshow("My Video", gray_frame_gau)
 
+
+    if first_frame is None:
+        first_frame = gray_frame_gau
+
+    delta_frame = cv2.absdiff(first_frame,gray_frame_gau) # absoulte difference between two frames
+
+    thresh_frame = cv2.threshold(delta_frame,60,255,cv2.THRESH_BINARY)[1]
+
+    dil_frame = cv2.dilate(thresh_frame,None,iterations=2)
+    cv2.imshow("My Video", dil_frame)
+
+    contours,check = cv2.findContours(dil_frame,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
+
+    for contour in contours:
+        if cv2.contourArea(contour)<10000:
+            continue
+        x,y,w,h = cv2.boundingRect(contour)
+        cv2.rectangle(frame,(x,y),(x+w,y+h))
     key = cv2.waitKey(1)
     if key == ord("q"):
         break

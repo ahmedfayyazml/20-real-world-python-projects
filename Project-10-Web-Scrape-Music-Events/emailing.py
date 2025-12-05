aa6020a126d8ff6343d9fa7b8f2d02dafded006c
Ahmed Fayyaz <awanahmed755@gmail.com>
2025-12-05 22:10:50 +0500

    Update emailing.py

--- Project-10-Web-Scrape-Music-Events/emailing.py
+++ Project-10-Web-Scrape-Music-Events/emailing.py
@@ -6,10 +6,10 @@ def send_email(message):
     port = 465
 
 
-    username = "babaralihaider74@gmail.com"
-    password = "cxlm olpn woct dlxm"
+    username = ""
+    password = ""
 
-    reciever = "babaralihaider74@gmail.com"
+    reciever = ""
     context = ssl.create_default_context()
 
     with smtplib.SMTP_SSL(host,port,context=context) as server:

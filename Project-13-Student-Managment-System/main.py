033bd5a7a562b81d2d1c3141be744cdad70c03be
ahmedfyaz <awanahmed755@gmail.com>
2025-12-11 21:41:42 +0500

    loaded data from the db into the table

--- Project-13-Student-Managment-System/main.py
+++ Project-13-Student-Managment-System/main.py
@@ -1,6 +1,6 @@
 from PyQt6.QtWidgets import (
     QApplication, QVBoxLayout, QLabel, QWidget,
-    QGridLayout, QLineEdit, QPushButton, QMainWindow, QTableWidget
+    QGridLayout, QLineEdit, QPushButton, QMainWindow, QTableWidget, QTableWidgetItem
 )
 from PyQt6.QtGui import  QAction
 import sys
@@ -23,12 +23,22 @@ class MainWindow(QMainWindow):
         self.table = QTableWidget()
         self.table.setColumnCount(4)
         self.table.setHorizontalHeaderLabels(("Id","Name","Course","Mobile"))
+        self.table.verticalHeader().setVisible(False)
         self.setCentralWidget(self.table)
 
     def load_data(self):
-        pass
+        connection = sqlite3.connect("database.db")
+        result = connection.execute("SELECT* FROM students")
+        self.table.setRowCount(0)
+        for row_number ,row_data in enumerate(result):
+            self.table.insertRow(row_number)
+            for column_number,data in enumerate(row_data):
+                self.table.setItem(row_number,column_number,QTableWidgetItem(str(data)))
+
+        print(result)
 
 app = QApplication(sys.argv)
 main_window = MainWindow()
 main_window.show()
+main_window.load_data()
 sys.exit(app.exec())

5afa7c547a8586366f63af5deb65a62d9ec27a01
ahmedfyaz <awanahmed755@gmail.com>
2025-12-21 23:28:44 +0500

    designed admin panel

--- Project-18-Django-Food-Delivery-App/restaurant_menu/models.py
+++ Project-18-Django-Food-Delivery-App/restaurant_menu/models.py
@@ -18,8 +18,8 @@ status = (
 class Item (models.Model):
     meal = models.CharField(max_length=1000,unique=True)
     description = models.CharField(max_length=2000)
-    price = models.DecimalField(decimal_places=2)
-    meal_type = models.CharField(choices=MEAL_TYPE)
+    price = models.DecimalField(max_digits=12,decimal_places=2)
+    meal_type = models.CharField(max_length=200,choices=MEAL_TYPE)
     author = models.ForeignKey(User,on_delete=models.PROTECT)
     status = models.IntegerField(choices=status,default=1)
     date_created = models.DateTimeField(auto_now_add=True)

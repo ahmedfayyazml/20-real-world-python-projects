01cc3c21973e92d45c79baa363430c911ea09146
ahmedfyaz <awanahmed755@gmail.com>
2025-12-21 22:35:58 +0500

    designed database model

--- Project-18-Django-Food-Delivery-App/restaurant_menu/models.py
+++ Project-18-Django-Food-Delivery-App/restaurant_menu/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+MEAL_TYPE = (
+    ('starters','Starters'),
+    ('salads','Salads'),
+    ('main_dishes','Main_Dishes'),
+    ('desserts','Desserts')
+)
+
+status = (
+    (0,'Unavailable'),
+    (1,'Available')
+)
+
+
+class Item (models.Model):
+    meal = models.CharField(max_length=1000,unique=True)
+    description = models.CharField(max_length=2000)
+    price = models.DecimalField(decimal_places=2)
+    meal_type = models.CharField(choices=MEAL_TYPE)
+    author = models.ForeignKey(User,on_delete=models.PROTECT)
+    status = models.IntegerField(choices=status,default=1)
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.meal

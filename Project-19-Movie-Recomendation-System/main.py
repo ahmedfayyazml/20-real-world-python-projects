af47aabf885b7adcf980ff4c11167b58d71fd5d9
ahmedfyaz <awanahmed755@gmail.com>
2025-12-23 22:24:20 +0500

    filtered data

--- Project-19-Movie-Recomendation-System/main.py
+++ Project-19-Movie-Recomendation-System/main.py
@@ -17,3 +17,17 @@ rating = pd.read_csv('ratings.csv')
 #
 # C- average rating across all movis
 
+m = movies['vote_count'].quantile(0.9)
+C = movies['vote_average'].mean()
+
+movies_filtered = movies.copy().loc[movies['vote_count']>=m]
+
+def weighted_rating(df,m=m,C=C):
+    R = df['vote_average']
+    v = df['vote_count']
+    wr = ((v/v+m)*R)+(m / (v+m) * C)
+    return wr
+
+movies_filtered['weighted_rating'] = movies_filtered.apply(weighted_rating,axis=1)
+
+
